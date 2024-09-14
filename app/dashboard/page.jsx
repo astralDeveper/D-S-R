1db39baa207996427f1895bd09aaad55fb759991
astralDeveper <astraldevelopers0@gmail.com>
2024-09-14 15:50:40 +0500

    dashboard top section done

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -33,21 +33,71 @@ const page = () => {
       tuitionCost : "$6,540/year",
       acceptanceRate : "44%"
     },
+    {
+      image: companyLogo,
+      head: "Florida Institute of Technnology",
+      web: "fsu.edu",
+      location: "fsu.edu",
+      description : "FSU, designated a preeminent university in the state of Florida, is one of the most respected research and learning institutions in the country.",
+      tuitionCost : "$6,540/year",
+      acceptanceRate : "44%"
+    },
+  ]
+
+  const cardArr2 = [
+    {
+      head: "USSSA World  Fastpitch Championship",
+      date: "07/14/2024 - 07/19/2024",
+      city: "Kanas City, KS"
+    },
+    {
+      head: "USSSA World  Fastpitch Championship",
+      date: "07/14/2024 - 07/19/2024",
+      city: "Kanas City, KS"
+    },
+    {
+      head: "USSSA World  Fastpitch Championship",
+      date: "07/14/2024 - 07/19/2024",
+      city: "Kanas City, KS"
+    },
+    {
+      head: "USSSA World  Fastpitch Championship",
+      date: "07/14/2024 - 07/19/2024",
+      city: "Kanas City, KS"
+    },
+    {
+      head: "USSSA World  Fastpitch Championship",
+      date: "07/14/2024 - 07/19/2024",
+      city: "Kanas City, KS"
+    },
+    {
+      head: "USSSA World  Fastpitch Championship",
+      date: "07/14/2024 - 07/19/2024",
+      city: "Kanas City, KS"
+    },
   ]
   return (
     <>
     <div className=''>
       <div className='mt-4 grid grid-cols-[55%_auto] gap-6 max-lg:grid-cols-1'>
-        <div className='flex flex-col gap-6'>
+        <div className=''>
           <h2 className='text-lg font-semibold'>Favorites</h2>
-          {cardArr.map((item, index)=>{
-            return <HomeCard image={item.image} head={item.head} description={item.description} web={item.web} location={item.location} tuitionCost={item.tuitionCost} acceptanceRate={item.acceptanceRate} key={index}/>
-          })}   
+          <div className='h-[900px] overflow-y-auto'>
+            <div className='flex flex-col gap-6 mt-6 '>
+              {cardArr.map((item, index)=>{
+                return <HomeCard image={item.image} head={item.head} description={item.description} web={item.web} location={item.location} tuitionCost={item.tuitionCost} acceptanceRate={item.acceptanceRate} key={index}/>
+              })}   
+            </div>
+          </div>
         </div>
-        <div className='flex flex-col gap-6'>
+        <div className=''>
           <h2 className='text-lg font-semibold'>My Events</h2>
-          <div>
-            <HomeCard2/>
+          <div className='h-[900px] overflow-y-auto mt-6'>
+            <div className='flex flex-col gap-6  '>
+              {cardArr2.map((item)=>{
+                return <HomeCard2/>
+              })}
+            </div>
           </div>
         </div>
       </div>
--- components/dashboard/HomeCard2.jsx
+++ components/dashboard/HomeCard2.jsx
@@ -4,7 +4,12 @@ const HomeCard2 = () => {
   return (
     <>
       <div className='bg-white p-6 rounded-[10px] border border-[#E9E9E9]'>
-        <h2 className='text-[#012555]'>USSSA World  Fastpitch Championship</h2>
+        <div className='flex items-center justify-between'>
+          <h2 className='text-[#012555] text-lg font-semibold'>USSSA World  Fastpitch Championship</h2>
+          <button className='text-[#E51A1A] text-lg font-semibold'>Edit</button>
+        </div>
+        <p className='text-xl mt-3'>07/14/2024 - 07/19/2024</p>
+        <p className='text-xl mt-3'>Kanas City, KS</p>
       </div>
     </>
   )
